0978dc9372ca033607779c6eab76293f36d9a4cb
antonscap <antonio.scape99@gmail.com>
2025-09-16 08:55:26 +0200

    test fix style on github pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@heroui/navbar";
-import { Avatar, Button, Link } from "@heroui/react";
+import { Avatar, Button, Image, Link } from "@heroui/react";
 
 export default function Nav() {
   return (
@@ -11,14 +11,15 @@ export default function Nav() {
             content: "bg-transparent",
             item: "bg-transparent",
         }}
+        className={"py-12 "}
     >
         <NavbarBrand className="text-2xl font-bold">
             <Button 
-            as={Link} 
-            radius={"none"}
-            href="/" 
-            startContent={<Avatar src="/logo_waveful_black.png" alt="Waveful" className={"bg-transparent"} size="lg" />}
-            className="text-4xl bg-transparent text-default-900 font-semibold"
+                as={Link} 
+                radius={"none"}
+                href="/" 
+                startContent={<Image src="/logo_waveful_colored.png" alt="Waveful" className={"bg-transparent h-24"} />}
+                className="text-4xl py-12 bg-transparent text-default-900 font-semibold"
             >
             Waveful
         </Button>
@@ -27,8 +28,9 @@ export default function Nav() {
             <NavbarItem>
                 <Button 
                     as={Link} 
-                    radius={"sm"}
+                    radius={"lg"}
                     href="/"
+                    size={"lg"}
                     className={"bg-default-900 text-default-100"}
                 >
                     Download the App
